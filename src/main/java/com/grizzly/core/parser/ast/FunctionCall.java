8fd4fc77d502604e31c723e1e58194be6d0cd732
shantanunp <shantanunp@gmail.com>
2026-02-28 22:13:17 +0530

    engine

--- src/main/java/com/grizzly/core/parser/ast/FunctionCall.java
+++ src/main/java/com/grizzly/core/parser/ast/FunctionCall.java
@@ -3,5 +3,4 @@ package com.grizzly.core.parser.ast;
 import java.util.List;
 
 public record FunctionCall(String functionName, List<Expression> args, int lineNumber) implements Statement {
-    // Record automatically provides lineNumber() accessor
 }
