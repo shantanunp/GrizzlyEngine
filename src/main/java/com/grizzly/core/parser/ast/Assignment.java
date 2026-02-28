8ed290cd7c874bd02d8f247ad9e871fc59526cdf
shantanunp <shantanunp@gmail.com>
2026-02-28 22:12:49 +0530

    engine

--- src/main/java/com/grizzly/core/parser/ast/Assignment.java
+++ src/main/java/com/grizzly/core/parser/ast/Assignment.java
@@ -1,5 +1,4 @@
 package com.grizzly.core.parser.ast;
 
 public record Assignment(Expression target, Expression value, int lineNumber) implements Statement {
-    // Record automatically provides lineNumber() accessor which satisfies the interface
 }
